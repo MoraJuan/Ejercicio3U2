6d43b37d27c89611e2dd2f48885330030e994584
MoraJuan <73558008+MoraJuan@users.noreply.github.com>
2022-04-25 19:05:14 -0300

    Add files via upload

--- Registro.py
+++ Registro.py
@@ -7,28 +7,40 @@ class Registro:
     
     def __init__(self,dia , hora,temperatura, humedad, presion):
         self.__dia = dia
-        self.__hora
+        self.__hora = hora
         self.__temperatura = temperatura
         self.__humedad = humedad
         self.__presion = presion
     def getTemperatura(self):
         return self.__temperatura
     def getDia(self):
-        return self.__temperatura
+        return self.__dia
     def getHora(self):
         return self.__hora
-    
+    def mostrar(self):
+        print (str(self.__dia)+','+str(self.__temperatura)+','+str(self.__humedad)+','+str(self.__presion))
     def TemperaturaMaxima(self, listaObjeto):
         maximo = -9999
         dia = None
         hora = None
         for i in range(len(listaObjeto)):
             for j in range(len(listaObjeto[i])):
-                if type(listaObjeto[i][j]) == Registro:
-                    if listaObjeto[i][j].getTemperatura() > str(maximo):
-                        maximo = listaObjeto[i][j].getTemperatura()
-                        dia = listaObjeto[i][j].getDia()
-                        hora = listaObjeto[i][j].getHora()
-        return 'Temperatura maxima registrada: {} en el dia {} hora {}'. format(maximo, dia, hora)
+                if listaObjeto[i][j].getTemperatura() > maximo:
+                    maximo = listaObjeto[i][j].getTemperatura()
+                    dia = listaObjeto[i][j].getDia()
+                    hora = listaObjeto[i][j].getHora()
+        print('Temperatura maxima registrada: {} en el dia {} hora {}'. format(maximo, dia, hora))
+    def TemperaturaMinima(self, listaObjeto):
+        minimo = 9999999
+        dia = None
+        hora = None
+        for i in range(len(listaObjeto)):
+            for j in range(len(listaObjeto[i])):
+                if listaObjeto[i][j].getTemperatura() < minimo:
+                    minimo = listaObjeto[i][j].getTemperatura()
+                    dia = listaObjeto[i][j].getDia()
+                    hora = listaObjeto[i][j].getHora()
+        print('Temperatura maxima registrada: {} en el dia {} hora {}'. format(minimo, dia, hora))
+        
    
 
