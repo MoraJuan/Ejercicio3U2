5d09de689d38d70223021ab3a22e8534d6aaa958
MoraJuan <73558008+MoraJuan@users.noreply.github.com>
2022-04-26 16:50:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from Registro import Registro
 import csv
+
 if __name__ == '__main__':
     listaObjeto=[]
-    
     
     for i in range(30):
         listaObjeto.append([0]*24)
@@ -14,12 +14,32 @@ if __name__ == '__main__':
         hora = int(fila[1])
         temperatura = float(fila[2])
         humedad = fila[3]
-        presion = fila[4]
+        presion = int(fila[4])
         UnRegistro = Registro(dia, hora, temperatura, humedad, presion)
         listaObjeto[dia-1][hora-1]=UnRegistro
+        
+    opcion=None
+    for objeto in listaObjeto:
+        while opcion != 'd':
+             print('\n-1- Mostrar para cada dia cada variable')
+             print('\n-2- Mostrar temperatura promedio para cada dia')
+             print('\n-3- Dado numero de dia mostrar cada variable')
+             print('\n-4- Salir')
+             opcion=input('\nIngrese opcion: ')
+             
+             if opcion == '1':
+                 UnRegistro.TemperaturaMaxima(listaObjeto)
+                 UnRegistro.TemperaturaMinima(listaObjeto)
+                 UnRegistro.HumedadMaxima(listaObjeto)
+                 UnRegistro.HumedadMinima(listaObjeto)
+                 UnRegistro.PresionMaxima(listaObjeto)
+                 UnRegistro.PresionMinima(listaObjeto)
+             elif opcion == '2':
+                 UnRegistro.TempPromedio(listaObjeto)
+             elif opcion == '3':
+                 xdia=int((input('Ingrese dia a mostrar: ')))
+                 UnRegistro.Mostrar(listaObjeto, xdia) 
+             elif opcion == '4':
+                 print('¡Good Bye!')
     
-        
-        
-    listaObjeto[4][8].mostrar()   
-    UnRegistro.TemperaturaMaxima(listaObjeto)
-    UnRegistro.TemperaturaMinima(listaObjeto)
+   
